03cbb7823cc3427f45b5d56567091678a57a71c6
beejsbj <burooj.bj@gmail.com>
2025-05-18 13:24:22 -0400

    Create Page section component
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

--- src/pages/ProductExplorerPage.tsx
+++ src/pages/ProductExplorerPage.tsx
@@ -1,11 +1,25 @@
-import { CartDrawer } from "@/layouts/CartDrawer.tsx";
-import ProductGrid from "@/layouts/ProductGrid.tsx";
+import CategoryHeader from '@/components/CategoryHeader/CategoryHeader'
+import { CartDrawer } from '@/layouts/CartDrawer.tsx'
+import PageSection from '@/layouts/PageSection'
+import ProductGrid from '@/layouts/ProductGrid.tsx'
+
+const content = {
+  title: 'Product Explorer',
+  description:
+    'This is a product explorer page. It is used to explore all products in the database. This is a test description. More text to test the truncation. Here you will find a list of products that are available in the database. Loading more text to test the truncation. Here you will find a list of products that are available in the database. Loading more text to test the truncation. Here you will find a list of products that are available in the database.'
+}
 
 export const ProductExplorerPage = () => {
-    return (
-        <>
-            <CartDrawer />
-            <ProductGrid />
-        </>
-    );
-};
+  return (
+    <>
+      <PageSection width="wide">
+        <CategoryHeader
+          title={content.title}
+          description={content.description}
+        />
+      </PageSection>
+
+      <ProductGrid />
+    </>
+  )
+}
